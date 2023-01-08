edc216ca1cb5bd40ef676c155945c3016c725031
Artem Stepanyuk <aivanov757@gmail.com>
2023-01-08 15:19:46 +0300

    4

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
+    assetModuleFilename: 'images/[name][contenthash][ext]'
   },
   module: {
     rules: [
